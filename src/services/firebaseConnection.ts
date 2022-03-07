2e9e414f9e33b5e98dafe00ca3233ff0ef4d7a3e
Will <dsilverfox98@gmail.com>
2022-03-07 12:55:24 -0300

    .env com informações do firebase feitas

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,17 +3,23 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 interface IFireConfig {
-    apiKey: string;
-    authDomain: string;
-    projectId: string;
-    storageBucket: string;
-    messagingSenderId: string;
-    appId: string;
-    measurementId: string;
+    apiKey: string | undefined;
+    authDomain: string | undefined;
+    projectId: string | undefined;
+    storageBucket: string | undefined;
+    messagingSenderId: string | undefined;
+    appId: string | undefined;
+    measurementId: string | undefined;
 }
 
 let firebaseConfig: IFireConfig = {
-
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
 }
 
 if (!firebase.apps.length) {
